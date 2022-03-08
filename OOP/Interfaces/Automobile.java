93696379329f5e9ad72009dd9fda071ce68db6f6
Paul Chheang <phalcheat@gmail.com>
2022-03-08 10:08:13 -0600

    Members/Fields in interface - They are public + static + final by default

--- OOP/Interfaces/Automobile.java
+++ OOP/Interfaces/Automobile.java
@@ -1,5 +1,10 @@
 public interface Automobile {
 
+    // NOTE: All methods in an interface are public and abstract by default
+    // we can omit the public modifier for interfaces. If we try to specify
+    // modifiers other than public it will show error
+    String safetyAssessmentProgram = "Global NCAP";
+
     String getMake();
 
     String getModel();
--- OOP/Interfaces/Main.java
+++ OOP/Interfaces/Main.java
@@ -1,16 +1,18 @@
 public class Main {
     public static void main(String[] args){
 
-        // Runtime polymorphism - it's highly recommended to make an instance variable
+        // Runtime polymorphism - it's highly recommended making an instance variable
         // instantiated from a class that implements an interface to be of that interface
         // type, NOT of that class type
-        Automobile sedan = new Sedan("Honda", "Civic", 21000.0);
+        Automobile honda = new Sedan("Honda", "Civic", 21000.0);
+        System.out.println("Honda safety program: " + honda.safetyAssessmentProgram);
 
-        System.out.println("Sedan make: " + sedan.getMake());
-        System.out.println("Sedan model: " + sedan.getModel());
-        System.out.println("Sedan price: " + sedan.getPrice());
+        Automobile toyota = new Sedan("Toyota", "Camry", 25000.0);
+        System.out.println("Toyota safety program: " + toyota.safetyAssessmentProgram);
 
-        System.out.println("sedan is an instance of Sedan: " + (sedan instanceof Sedan));
-        System.out.println("sedan is an instance of Automobile: " + (sedan instanceof Automobile));
+        // Member variables of an interface are "public static final" by default
+        System.out.println("Automobile safety program: " + Automobile.safetyAssessmentProgram);
+        System.out.println("Sedan safety program: " + Sedan.safetyAssessmentProgram);
+
     }
 }
