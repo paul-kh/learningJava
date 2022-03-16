7f2c6419fa23d2d2f9ff49405d9fb9a990ff8853
Paul Chheang <phalcheat@gmail.com>
2022-03-16 16:05:24 -0500

    Handling exception by throwing exception in a method signature rather than using try-catch block

--- ExploringExceptions/src/ExploringExceptions.java
+++ ExploringExceptions/src/ExploringExceptions.java
@@ -2,6 +2,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class ExploringExceptions {
+
+    /* Exception handling using try-catch block
     public static void main(String[] args) {
 
         try {
@@ -13,5 +15,18 @@ public class ExploringExceptions {
             System.out.println("code execution still continues after the exception thrown");
             e.printStackTrace(); // print out Exception stack
         }
+    }
+
+     */
+
+    // Exception handling by throwing exception in the method signature
+    public static void main(String[] args) throws FileNotFoundException {
+
+            // The file "myFile.txt" doesn't exist
+            FileReader file = new FileReader("D:\\java\\ExploringExceptions\\data\\myfile.txt");
+            // Note - Java will auto catch the exception and print exception stack.
+            //        Next line of code will not be executed
+            System.out.println("code execution here can't continues after the exception thrown");
+
     }
 }
