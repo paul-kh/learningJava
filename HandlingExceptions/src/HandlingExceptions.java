2509540f905a4f99c0c80fd2b28fcbe97b27a006
Paul Chheang <phalcheat@gmail.com>
2022-03-17 14:55:06 -0500

    Catching a specific Exception with try-catch block

--- HandlingExceptions/src/HandlingExceptions.java
+++ HandlingExceptions/src/HandlingExceptions.java
@@ -13,26 +13,9 @@ public class HandlingExceptions {
         try {
             System.out.format("\nThe price at index %s is %f\n", index, prices[index]);
         }
-        catch (Exception e) { // Catch a generic Exception
-            System.out.println("An exception occurred");
-            // e.printStackTrace(); // print the call stack to show the involved classes
-            // System.out.println(e);
+        catch (ArrayIndexOutOfBoundsException e) { // Catch a specific Exception
 
-            System.out.println("The Exception class: " + e.getClass());
-            System.out.println("The Exception message: " + e.getMessage());
-            System.out.println("The Exception localized message: " + e.getLocalizedMessage());
-            System.out.println("The Exception cause: " + e.getCause());
-
-            System.out.println("\nExploring more about the Exception object ===========================");
-            System.out.println("Is e an Object? " + (e instanceof Object));
-            System.out.println("Is e a Throwable? " + (e instanceof Throwable));
-            System.out.println("Is e an Exception? " + (e instanceof Exception));
-            System.out.println("Is e a RuntimeException? " + (e instanceof RuntimeException));
-            System.out.println("Is e an ArrayIndexOutOfBoundsException? " + (e instanceof ArrayIndexOutOfBoundsException));
-
-            System.out.println("Is e an ArithmeticException? " + (e instanceof ArithmeticException));
-            System.out.println("Is e a StringIndexOutOfBoundsException? " + (e instanceof StringIndexOutOfBoundsException));
-
+            System.out.println("An exception occurred: " + e.getClass());
         }
 
         System.out.println("End of the program");
