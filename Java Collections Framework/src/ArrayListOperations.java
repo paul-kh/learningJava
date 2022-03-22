4de3505d1a65396b0345b699fec5291cee1ea25a
Paul Chheang <phalcheat@gmail.com>
2022-03-22 12:35:00 -0500

    more List operations - .get(), .indexOf(), . lastIndexOf(), .set(), .sublist()

--- Java Collections Framework/src/ArrayListOperations.java
+++ Java Collections Framework/src/ArrayListOperations.java
@@ -4,23 +4,7 @@ import java.util.ArrayList;
 
 public class ArrayListOperations {
     public static void main(String[] args) {
-
-        ArrayList<Integer> intList = new ArrayList<>(); //List accepts Object Type but not primitive type
-        // ArrayList<int>.... will give Compile Time Error
-
-        intList.add(10);
-        intList.add(20);
-        intList.add(30);
-
-        System.out.println("Integer List: " + intList);
-
-        intList.add(null);
-
-        System.out.println("Integer List (after adding null): " + intList);
-
-        ///////////////////////////////////////////////////////////////
-        // String Object Operations -- City List
-        ArrayList<String> cityList = new ArrayList<>();
+        List<String> cityList = new ArrayList<>();
 
         System.out.println("**************Adding elements");
 
@@ -30,72 +14,126 @@ public class ArrayListOperations {
 
         System.out.println("cityList contents: " + cityList);
         System.out.println("cityList size: " + cityList.size());
-        System.out.println("cityList isEmpty(): " + cityList.isEmpty());
+
+        System.out.println();
+
+        // Run
+
+        System.out.println("**************Adding elements at specified index");
+
+        cityList.add(0, "Seattle"); // this will append list before current city of index 0 "Bengaluru"
+
+        System.out.println("cityList contents: " + cityList);
+        System.out.println("cityList size: " + cityList.size());
+
+        cityList.add(2, "Palo Alto"); // this will append list before current city of index 2
+
+        System.out.println("cityList contents: " + cityList);
+        System.out.println("cityList size: " + cityList.size());
+
+        System.out.println();
+
+        // Run
+
+        System.out.println("**************Accessing elements at the specified index");
+
+        System.out.println("Element at index 0: " + cityList.get(0));
+        System.out.println("Element at index 2: " + cityList.get(2));
+        System.out.println("Element at index 4: " + cityList.get(4));
+
+        // Run
+
+//        System.out.println("Element at index 7: " + cityList.get(7));
+
+        // Run - this will be an error IndexOutOfBoundsException
+        // -- Comment out the line in error and continue
+
+        System.out.println("**************Removing elements from specified index");
+
+        System.out.println("cityList original contents: " + cityList);
+
+        cityList.remove(0);
+
+        System.out.println("cityList contents after removing city of index 0: " + cityList);
+        System.out.println("cityList size: " + cityList.size());
+
+        // Run
+
+        cityList.remove(3);
+
+        System.out.println("cityList contents after removing city of index 3: " + cityList);
+        System.out.println("cityList size: " + cityList.size());
+
+        System.out.println();
+
+        // Run
+
+        System.out.println("**************Lists can contain duplicates");
+        System.out.println("cityList original contents: " + cityList);
+
+        cityList.add("Palo Alto");
+        cityList.add("Bengaluru");
+        cityList.add("Mumbai");
+        cityList.add("Seattle");
+
+        System.out.println("cityList contents: " + cityList);
 
         System.out.println();
 
         // Run
 
         System.out.println("**************Removing elements");
-
-        cityList.remove("Seattle");
-        cityList.remove("Palo Alto");
-
-        System.out.println("cityList contents: " + cityList);
-        System.out.println("cityList size: " + cityList.size());
-        System.out.println("cityList isEmpty(): " + cityList.isEmpty());
-
-        System.out.println();
-
-        // Run
-
-        System.out.println("**************Checking for elements");
-
-        System.out.println("cityList contains New York: " + cityList.contains("New York"));
-        System.out.println("cityList contains Paris: " + cityList.contains("Paris"));
-        System.out.println("cityList contains Mumbai: " + cityList.contains("Mumbai"));
-
-        System.out.println("cityList size: " + cityList.size());
-
-        System.out.println();
-
-        // Run
-
-        System.out.println("**************Adding multiple elements");
-
-        List<String> anotherCityList = new ArrayList<>();
-        anotherCityList.add("Paris");
-        anotherCityList.add("London");
-
         System.out.println("cityList original contents: " + cityList);
-        System.out.println("Adding elements: " + anotherCityList);
 
-        cityList.addAll(anotherCityList);
-
+        cityList.remove(2);
         System.out.println("cityList contents: " + cityList);
-        System.out.println("cityList size: " + cityList.size());
 
         System.out.println();
 
         // Run
 
-        System.out.println("**************Checking for multiple elements");
+        System.out.println("**************Find the index position of an element");
+        System.out.println("cityList original contents: " + cityList);
 
-        System.out.println("cityList contains Paris, London: " + cityList.containsAll(anotherCityList));
+        // NOTE: That this finds the first position of the element from the beginning of the list
+        System.out.println("Mumbai is at index: " + cityList.indexOf("Mumbai"));
+        System.out.println("Palo Alto is at index: " + cityList.indexOf("Palo Alto"));
+        System.out.println("Paris is at index: " + cityList.indexOf("Paris"));
 
-        anotherCityList.add("Tokyo");
-        System.out.println("cityList contains Paris, London, Tokyo: " + cityList.containsAll(anotherCityList));
+        System.out.println();
 
         // Run
 
-        System.out.println("**************Removing multiple elements");
-
+        System.out.println("**************Find the last index position of an element");
         System.out.println("cityList original contents: " + cityList);
-        System.out.println("Removing elements: " + anotherCityList);
 
-        cityList.removeAll(anotherCityList);
+        // NOTE: That this finds the last position of the element from the beginning of the list
+        System.out.println("Mumbai is at index: " + cityList.lastIndexOf("Mumbai"));
+        System.out.println("Bengaluru is at index: " + cityList.lastIndexOf("Bengaluru"));
+        System.out.println("Palo Alto is at index: " + cityList.lastIndexOf("Palo Alto"));
+
+        System.out.println();
+
+        // Run
+
+        System.out.println("**************Update an element at a specified index");
+        System.out.println("cityList original contents: " + cityList);
+
+        cityList.set(0, "Tokyo");
+        cityList.set(2, "Moscow");
 
         System.out.println("cityList contents: " + cityList);
+
+        System.out.println();
+
+        // Run
+
+        System.out.println("**************Access a sublist from the original list");
+        System.out.println("cityList original contents: " + cityList);
+
+        List<String> subList = cityList.subList(1, 4);
+
+        System.out.println("subList contents: " + subList);
 
     }
 }
