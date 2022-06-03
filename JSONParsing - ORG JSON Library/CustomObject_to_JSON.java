614b607464bbb636d5536008ab4771aa63740a51
Paul Chheang <phalcheat@gmail.com>
2022-06-03 11:33:55 -0500

    Add custom/Bean objects to JSON Array

--- JSONParsing - ORG JSON Library/CustomObject_to_JSON.java
+++ JSONParsing - ORG JSON Library/CustomObject_to_JSON.java
@@ -1,8 +1,11 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomObject_to_JSON {
     public static void main(String[] args) {
@@ -23,6 +26,28 @@ public class CustomObject_to_JSON {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+        // Add list of employees in JSON format to JSONArray
+        EmployeeBean employeeBean1 = new EmployeeBean(BigInteger.valueOf(1234568), "Peter", "CEO", 150000, 30);
+
+        EmployeeBean employeeBean2 = new EmployeeBean();
+        employeeBean2.setId(BigInteger.valueOf(1234569));
+        employeeBean2.setName("Peter");
+        employeeBean2.setTitle("CEO");
+
+        EmployeeBean employeeBean3 = new EmployeeBean();
+        employeeBean3.setId(BigInteger.valueOf(1234570));
+        employeeBean3.setName("Tana");
+
+        List<EmployeeBean> employeeBeanList = new ArrayList<>();
+        employeeBeanList.add(employeeBean1);
+        employeeBeanList.add(employeeBean2);
+        employeeBeanList.add(employeeBean3);
+
+        JSONArray employeeJsonList = new JSONArray(employeeBeanList);
+
+        System.out.println(employeeJsonList.toString(2));
 
     }
 }
