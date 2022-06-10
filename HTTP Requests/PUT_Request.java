538dfd5e9ecaa32a4410c2898580ee10b238e8a7
Paul Chheang <phalcheat@gmail.com>
2022-06-10 14:07:20 -0500

    PUT request

---
+++ HTTP Requests/PUT_Request.java
@@ -0,0 +1,38 @@
+package com.paulchheang.http_request;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+public class PUT_Request {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        String url = "https://reqres.in/api/users/563";
+
+        //POST data needs to be of JSON Format
+        String postData = "{'email': 'alice.thompson@paulchheang.com'," +
+                "'firstName': 'Alice', " +
+                "'lastName': 'Thompson'}";
+
+        //Create HTTP Client object
+        HttpClient client = HttpClient.newBuilder().build();
+
+        //Create POST request
+        HttpRequest req = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .PUT(HttpRequest.BodyPublishers.ofString(postData))
+                .build();
+        //Send out the request object to API end point
+        HttpResponse<?> res = client.send(req, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println("Status:" + res.statusCode());
+        System.out.println("Headers:" + res.headers());
+
+        //Parsing response object to JSON object
+        JSONObject resJson =  new JSONObject(res.body().toString());
+        System.out.println("Response JSON: \n" + resJson.toString(4));
+    }
+}
