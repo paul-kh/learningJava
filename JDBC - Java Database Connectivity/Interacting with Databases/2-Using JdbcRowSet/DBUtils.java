9e0ef8260e48cd58e61d884ba85e92f937781442
Paul Chheang <phalcheat@gmail.com>
2022-06-13 16:02:16 -0500

    JdbcRowSet - Navigating rows in JdbcRowSet

--- JDBC - Java Database Connectivity/Interacting with Databases/2-Using JdbcRowSet/DBUtils.java
+++ JDBC - Java Database Connectivity/Interacting with Databases/2-Using JdbcRowSet/DBUtils.java
@@ -1,10 +1,7 @@
 package com.paulchheang.jdbc;
 
-import com.mysql.cj.jdbc.MysqlDataSource; //this takes care of registering Driver, and connection pooling
-
 import javax.sql.rowset.JdbcRowSet;
 import javax.sql.rowset.RowSetProvider;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DBUtils {
@@ -12,28 +9,6 @@ public class DBUtils {
     private static final String dbURL = "jdbc:mysql://localhost:3306/";
     private static final String username = "root";
     private static final String password = "root";
-
-    public static Connection getMysqlConnection(String schemaName) {
-
-        MysqlDataSource mysqlDS = null;
-        Connection conn = null;
-
-        try {
-            mysqlDS = new MysqlDataSource();
-
-            mysqlDS.setURL(dbURL + schemaName);
-            mysqlDS.setUser(username);
-            mysqlDS.setPassword(password);
-
-            conn = mysqlDS.getConnection();
-        }
-        catch (SQLException ex) {
-
-            ex.printStackTrace();
-        }
-
-        return conn;
-    }
 
     //Method defining JdbcRowSet with Data Source connection encapsulated
     public static JdbcRowSet getJdbcRowSet(String schemaName){
--- JDBC - Java Database Connectivity/Interacting with Databases/2-Using JdbcRowSet/Exploring_JdbcRowSet.java
+++ JDBC - Java Database Connectivity/Interacting with Databases/2-Using JdbcRowSet/Exploring_JdbcRowSet.java
@@ -3,22 +3,47 @@ package com.paulchheang.jdbc;
 import javax.sql.rowset.JdbcRowSet;
 import java.sql.SQLException;
 
+import javax.sql.RowSet;
+
 public class Exploring_JdbcRowSet {
 
-    public static void main(String[] args) {
+    public static void displayRow(String label, RowSet rowSet) throws SQLException{
 
+        String fName = rowSet.getString("first_name");
+        String lName = rowSet.getString("last_name");
+        double hourlyRate = rowSet.getDouble("hourly_rate");
+        boolean isFT = rowSet.getBoolean("is_fulltime");
+
+        String stdData = "\n%s:  %s | %s | %.2f | %s \n";
+        System.out.format(stdData, label, fName, lName, hourlyRate, isFT);
+    }
+
+    public static void main(String[] args) {
 
         try (JdbcRowSet jdbcRS = DBUtils.getJdbcRowSet("DeliveryService")){
 
             jdbcRS.setCommand("select * from delpartners");
             jdbcRS.execute();
 
-           while (jdbcRS.next()) {
-               System.out.println(jdbcRS.getString("first_name")
-                       + "\t" + jdbcRS.getString("last_name")+ "\t"
-                       + jdbcRS.getDouble("hourly_rate")
-                       + "\t" + jdbcRS.getBoolean("is_fulltime"));
-           }
+            System.out.println("Moving around in a JdbcRowSet: \n");
+
+            jdbcRS.first();
+            displayRow("first()", jdbcRS);
+
+            jdbcRS.relative(2);
+            displayRow("relative(2)", jdbcRS);
+
+            jdbcRS.previous();
+            displayRow("previous()", jdbcRS);
+
+            jdbcRS.absolute(4);
+            displayRow("absolute(4)", jdbcRS);
+
+            jdbcRS.last();
+            displayRow("last()", jdbcRS);
+
+            jdbcRS.relative(-1); //Similar to jdbcRS.previous();
+            displayRow("relative(-1)", jdbcRS);
 
         }
         catch (SQLException ex) {
