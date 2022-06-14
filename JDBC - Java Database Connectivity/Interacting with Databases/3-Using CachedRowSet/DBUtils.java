6283b0317ac1e0a6970a0e34b5011538d783a032
Paul Chheang <phalcheat@gmail.com>
2022-06-14 12:56:45 -0500

    CachedRowSet - UPDATE rows in DB table data

--- JDBC - Java Database Connectivity/Interacting with Databases/3-Using CachedRowSet/DBUtils.java
+++ JDBC - Java Database Connectivity/Interacting with Databases/3-Using CachedRowSet/DBUtils.java
@@ -1,15 +1,41 @@
 package com.paulchheang.jdbc;
 
-import javax.sql.rowset.CachedRowSet;
-import javax.sql.rowset.JdbcRowSet;
-import javax.sql.rowset.RowSetProvider;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.sql.Connection;
 import java.sql.SQLException;
+import javax.sql.rowset.RowSetProvider;
+import javax.sql.rowset.CachedRowSet;
 
 public class DBUtils {
 
     private static final String dbURL = "jdbc:mysql://localhost:3306/";
     private static final String username = "root";
     private static final String password = "root";
+
+    //Create a Connection object to DB
+    public static Connection getMysqlConnection(String schemaName){
+
+        MysqlDataSource mysqlDS = null;
+        Connection conn = null;
+
+        try {
+            mysqlDS = new MysqlDataSource();
+
+            mysqlDS.setURL("jdbc:mysql://localhost:3306/" + schemaName);
+            mysqlDS.setUser(username);
+            mysqlDS.setPassword(password);
+
+            conn = mysqlDS.getConnection();
+        }
+        catch (SQLException ex) {
+
+            ex.printStackTrace();
+        }
+
+        return conn;
+    }
+
 
     //Method defining CachedRowSet with Data Source connection encapsulated
     public static CachedRowSet getCachedRowSet(String schemaName){
--- JDBC - Java Database Connectivity/Interacting with Databases/3-Using CachedRowSet/Exploring_CachedRowSet.java
+++ JDBC - Java Database Connectivity/Interacting with Databases/3-Using CachedRowSet/Exploring_CachedRowSet.java
@@ -1,27 +1,57 @@
 package com.paulchheang.jdbc;
 
+import javax.sql.RowSet;
 import javax.sql.rowset.CachedRowSet;
+import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Exploring_CachedRowSet {
 
+    public static void displayRow(String label, RowSet rowSet) throws SQLException{
+
+        String fName = rowSet.getString("first_name");
+        String lName = rowSet.getString("last_name");
+        double hourlyRate = rowSet.getDouble("hourly_rate");
+        boolean isFT = rowSet.getBoolean("is_fulltime");
+
+        String stdData = "\n%s:  %s | %s | %.2f | %s \n";
+        System.out.format(stdData, label, fName, lName, hourlyRate, isFT);
+    }
+
     public static void main(String[] args) {
 
-        try (CachedRowSet cachedRS = DBUtils.getCachedRowSet("DeliveryService")){
+        //Using Try resource block to create a Connection object (Resource auto closed after Try block)
+        try (Connection conn = DBUtils.getMysqlConnection("DeliveryService")){
 
-            System.out.println("Retrieving all Delivery Partner who are fulltime employee...\n");
+            conn.setAutoCommit(false);
 
-            cachedRS.setCommand("select * from delpartners where is_fulltime = ?");
-            cachedRS.setBoolean(1, true);
-            cachedRS.execute();
+            CachedRowSet cachedRS = DBUtils.getCachedRowSet(""); //no schema name
 
-            while (cachedRS.next()) {
-                System.out.println(cachedRS.getString("first_name")
-                        + "\t" + cachedRS.getString("last_name")+ "\t"
-                        + cachedRS.getDouble("hourly_rate")
-                        + "\t" + cachedRS.getBoolean("is_fulltime"));
+            cachedRS.setCommand("select * from delpartners");
+            cachedRS.execute(conn);
+
+            int updatedRows = 0;
+
+            while(cachedRS.next()){
+
+                if(!cachedRS.getBoolean("is_fulltime")){
+
+                    cachedRS.updateDouble("hourly_rate", 26.0);
+                    cachedRS.updateRow();
+
+                    displayRow("Updated record: ", cachedRS);
+                    updatedRows++;
+                }
             }
+
+            cachedRS.acceptChanges(conn); //Push through to DB using the currently open Connection
+
+            cachedRS.close(); //Close the CachedRowSet object resource
+
+            System.out.println("\nNumber of updated rows: " + updatedRows);
+
         }
+
         catch (SQLException ex) {
             ex.printStackTrace();
         }
