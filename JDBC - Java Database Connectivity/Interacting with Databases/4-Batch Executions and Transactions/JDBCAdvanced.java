5f9c07f221f3a4a3244539169576b58a47075bd7
Paul Chheang <phalcheat@gmail.com>
2022-06-15 16:26:40 -0500

    Created Savepoints in Transactions so partial queries can get executed when there is any failed query execution in the transactions

--- JDBC - Java Database Connectivity/Interacting with Databases/4-Batch Executions and Transactions/JDBCAdvanced.java
+++ JDBC - Java Database Connectivity/Interacting with Databases/4-Batch Executions and Transactions/JDBCAdvanced.java
@@ -2,11 +2,12 @@ package com.paulchheang.jdbc;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Savepoint;
 import java.sql.Statement;
 
-//The code is to run queries in Transactions
-//This is to make sure all queries will execute successfully or none of them will succeed
-// if one query failed to execute
+//The code is to run queries in Transactions with savepoints
+//This is to partially commit the successfully executed queries to DB
+// and rollback the failed queries execution based on their savepoints
 public class JDBCAdvanced {
 
     public static void main(String[] args) throws SQLException {
@@ -14,29 +15,57 @@ public class JDBCAdvanced {
         Statement stmnt;
         String query;
         Connection conn = null;
+        Savepoint sp = null;
 
         try{
 
             conn = DBUtils.getMysqlConnection("DeliveryService");
-            conn.setAutoCommit(false); //Turn off auto commit
+            conn.setAutoCommit(false);
 
             stmnt = conn.createStatement();
 
-            //The below query is made to get successful execution since the partner with id=107 not exist yet
-            query = "insert into delpartners values (107, 'Kylie', 'Kass', 22.0, false)";
+            query = "insert into delpartners values (106, 'Brian', 'Walters', 22.0, false)"; //Valid query
             stmnt.executeUpdate(query);
 
-            //The below query is made to get fail execution since vehicle with id=16 already exists
-            query = "insert into delvehicles values (16, 'Orange', 'Van', 'LOONY17')";
+            query = "insert into delvehicles values (16, 'Grey', 'Van', 'LOONY18')"; //Valid query
             stmnt.executeUpdate(query);
 
-            conn.commit(); //Commit to DB after the last query
+            sp = conn.setSavepoint("OnePlusOne");
+            System.out.println("Save point created: " + sp.getSavepointName());
+
+            query = "insert into delvehicles values (19, 'Pink', 'Truck', 'LOONY19')"; //valid query
+            stmnt.executeUpdate(query);
+
+            query = "insert into delpartners values (106, 'Aisha', 'Hussain', 22.0, false)"; //invalid since id=106 exists
+            stmnt.executeUpdate(query);
+
+            //The below code will never get hit since the exception occured at the above invalid query
+            sp = conn.setSavepoint("TwoPlusTwo");
+            System.out.println("Save point created: " + sp.getSavepointName());
+
+            query = "insert into delvehicles values (20, 'Yellow', 'Truck', 'LOONY20')"; //Valid query but will not get executed
+            stmnt.executeUpdate(query);
+
+            query = "insert into delpartners values (110, 'Cuthbert', 'Crumble', 22.0, false)"; //Valid query but will not get executed
+            stmnt.executeUpdate(query);
+
+            conn.commit();
             System.out.println("Rows have been successfully inserted");
 
         }
         catch (SQLException ex) {
-            System.out.println("An exception was thrown. Rolling back the Tx...");
-            conn.rollback(); //Rollback log data that might have written when any query execution failed
+
+            if(sp != null){
+
+                System.out.println("An exception was thrown. Rolling back to " + sp.getSavepointName());
+                conn.rollback(sp); //rollback at the savepoint
+                conn.commit();
+            }
+            else{
+                System.err.println("Errors detected. Rolling back everything...");
+                conn.rollback();
+            }
+
             ex.printStackTrace();
         }
         finally {
