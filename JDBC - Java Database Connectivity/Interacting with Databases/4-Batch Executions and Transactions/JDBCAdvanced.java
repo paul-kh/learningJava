132de6851bc4ccfac5a50d58eee09e57fce45f51
Paul Chheang <phalcheat@gmail.com>
2022-06-15 16:41:49 -0500

    Created savepoints in Batch execution of INSERT queries

--- JDBC - Java Database Connectivity/Interacting with Databases/4-Batch Executions and Transactions/JDBCAdvanced.java
+++ JDBC - Java Database Connectivity/Interacting with Databases/4-Batch Executions and Transactions/JDBCAdvanced.java
@@ -1,56 +1,46 @@
 package com.paulchheang.jdbc;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Savepoint;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Arrays;
 
-//The code is to run queries in Transactions with savepoints
-//This is to partially commit the successfully executed queries to DB
-// and rollback the failed queries execution based on their savepoints
+/*
+This program is to run queries in 2 batches with savepoints
+All queries in the first batch will commit since no query in this batch is invalid
+All queries in the 2nd batch will commit since there is one invalid query */
 public class JDBCAdvanced {
 
     public static void main(String[] args) throws SQLException {
 
-        Statement stmnt;
-        String query;
+        int[] count = {};
         Connection conn = null;
         Savepoint sp = null;
 
         try{
 
             conn = DBUtils.getMysqlConnection("DeliveryService");
-            conn.setAutoCommit(false);
+            conn.setAutoCommit(false); //Turn off auto commit
 
-            stmnt = conn.createStatement();
+            PreparedStatement insertPS = DBUtils.getInsertVehiclePS(conn);
 
-            query = "insert into delpartners values (106, 'Brian', 'Walters', 22.0, false)"; //Valid query
-            stmnt.executeUpdate(query);
+            //First batch - all queries will get committed
+            DBUtils.addToInsertVehicleBatch(insertPS, 16, "Red", "Truck", "LOONY16");
+            DBUtils.addToInsertVehicleBatch(insertPS, 17, "Orange", "Van", "LOONY17");
+            DBUtils.addToInsertVehicleBatch(insertPS, 18, "Grey", "Van", "LOONY18");
+            DBUtils.addToInsertVehicleBatch(insertPS, 19, "Pink", "Truck", "LOONY19");
 
-            query = "insert into delvehicles values (16, 'Grey', 'Van', 'LOONY18')"; //Valid query
-            stmnt.executeUpdate(query);
+            count = insertPS.executeBatch();
+            sp = conn.setSavepoint("BatchOne");
+            System.out.println("First batch executed: " + Arrays.toString(count));
 
-            sp = conn.setSavepoint("OnePlusOne");
-            System.out.println("Save point created: " + sp.getSavepointName());
+            // 2nd batch - all queries will not get committed because there's one invalid query
+            DBUtils.addToInsertVehicleBatch(insertPS, 20, "Yellow", "Truck", "LOONY20");
+            DBUtils.addToInsertVehicleBatch(insertPS, 19, "Pink", "Truck", "LOONY19"); //Invalid since id=19 exists
+            DBUtils.addToInsertVehicleBatch(insertPS, 21, "Green", "Van", "LOONY21");
 
-            query = "insert into delvehicles values (19, 'Pink', 'Truck', 'LOONY19')"; //valid query
-            stmnt.executeUpdate(query);
-
-            query = "insert into delpartners values (106, 'Aisha', 'Hussain', 22.0, false)"; //invalid since id=106 exists
-            stmnt.executeUpdate(query);
-
-            //The below code will never get hit since the exception occured at the above invalid query
-            sp = conn.setSavepoint("TwoPlusTwo");
-            System.out.println("Save point created: " + sp.getSavepointName());
-
-            query = "insert into delvehicles values (20, 'Yellow', 'Truck', 'LOONY20')"; //Valid query but will not get executed
-            stmnt.executeUpdate(query);
-
-            query = "insert into delpartners values (110, 'Cuthbert', 'Crumble', 22.0, false)"; //Valid query but will not get executed
-            stmnt.executeUpdate(query);
+            count = insertPS.executeBatch();
+            System.out.println("Second batch executed: " + Arrays.toString(count));
 
             conn.commit();
-            System.out.println("Rows have been successfully inserted");
 
         }
         catch (SQLException ex) {
@@ -58,8 +48,8 @@ public class JDBCAdvanced {
             if(sp != null){
 
                 System.out.println("An exception was thrown. Rolling back to " + sp.getSavepointName());
-                conn.rollback(sp); //rollback at the savepoint
-                conn.commit();
+                conn.rollback(sp); //Rollback to the savepoint created
+                conn.commit(); //commit all queries upto the savepoint created
             }
             else{
                 System.err.println("Errors detected. Rolling back everything...");
