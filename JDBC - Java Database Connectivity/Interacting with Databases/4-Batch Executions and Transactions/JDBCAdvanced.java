e0454983905daa2390de4cee4f3efc266b8e73db
Paul Chheang <phalcheat@gmail.com>
2022-06-15 16:02:20 -0500

    Execute multiple INSERT queries in Transaction mode

--- JDBC - Java Database Connectivity/Interacting with Databases/4-Batch Executions and Transactions/JDBCAdvanced.java
+++ JDBC - Java Database Connectivity/Interacting with Databases/4-Batch Executions and Transactions/JDBCAdvanced.java
@@ -1,32 +1,46 @@
 package com.paulchheang.jdbc;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Arrays;
 
+//The code is to run queries in Transactions
+//This is to make sure all queries will execute successfully or none of them will succeed
+// if one query failed to execute
 public class JDBCAdvanced {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         Statement stmnt;
+        String query;
+        Connection conn = null;
 
-        try (Connection conn = DBUtils.getMysqlConnection("DeliveryService")){
+        try{
 
-            PreparedStatement insertPS = DBUtils.getInsertVehiclePS(conn);
+            conn = DBUtils.getMysqlConnection("DeliveryService");
+            conn.setAutoCommit(false); //Turn off auto commit
 
-            DBUtils.addToInsertVehicleBatch(insertPS, 16, "Red", "Truck", "CPAUL16");
-            DBUtils.addToInsertVehicleBatch(insertPS, 17, "Orange", "Van", "CPAUL17");
-            DBUtils.addToInsertVehicleBatch(insertPS, 18, "Grey", "Van", "CPAUL18");
-            DBUtils.addToInsertVehicleBatch(insertPS, 19, "Pink", "Truck", "CPAUL19");
+            stmnt = conn.createStatement();
 
-            int[] count = insertPS.executeBatch();
+            //The below query is made to get successful execution since the partner with id=107 not exist yet
+            query = "insert into delpartners values (107, 'Kylie', 'Kass', 22.0, false)";
+            stmnt.executeUpdate(query);
 
-            System.out.println("Values inserted successfully: \n" + Arrays.toString(count));
+            //The below query is made to get fail execution since vehicle with id=16 already exists
+            query = "insert into delvehicles values (16, 'Orange', 'Van', 'LOONY17')";
+            stmnt.executeUpdate(query);
+
+            conn.commit(); //Commit to DB after the last query
+            System.out.println("Rows have been successfully inserted");
+
         }
         catch (SQLException ex) {
+            System.out.println("An exception was thrown. Rolling back the Tx...");
+            conn.rollback(); //Rollback log data that might have written when any query execution failed
             ex.printStackTrace();
+        }
+        finally {
+            conn.close();
         }
     }
 }
